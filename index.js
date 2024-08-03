d473fa7ec02823ed18e9c114af8cdb5fcbcd631b
admin <piccolisantiagonicolas@gmail.com>
2024-08-02 22:18:56 -0300

    fixed the reset function. did some format rearreangement to allfiles

--- index.js
+++ index.js
@@ -5,35 +5,49 @@ let timer = null;
 let startTime = 0;
 let elapsedTime = 0;
 
-function start(){
-    if(!isRunning){
-        startTime = Date.now() - elapsedTime;
-        timer = setInterval(update,5);
-        console.log(timer);
-        isRunning = true;
-    }
+function start() {
+  if (!isRunning) {
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(update, 5);
+    console.log(timer);
+    isRunning = true;
+  }
 }
-function end(){
-    if(isRunning){
-        clearInterval(timer);
-        elapsedTime = Date.now() - startTime;
-        isRunning = false;
-    }
-}
-function reset(){
+
+function pause() {
+  if (isRunning) {
     clearInterval(timer);
     elapsedTime = Date.now() - startTime;
     isRunning = false;
+  }
 }
 
-function update(){
-    const currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
-    let days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24)).toString().padStart(2, 0);
-    let hours = Math.floor(elapsedTime / (1000 * 60 *60)).toString().padStart(2, 0);
-    let minutes = Math.floor(elapsedTime / (1000 * 60 ) % 60).toString().padStart(2, 0);
-    let seconds = Math.floor(elapsedTime / 1000 % 60).toString().padStart(2, 0);
-    let miliseconds = Math.floor(elapsedTime % 1000 / 10).toString().padStart(2, 0);
+function reset() {
+  clearInterval(timer);
+  startTime = 0;
+  elapsedTime = 0;
+  isRunning = false;
+  display.textContent = "00:00:00:00:00";
+}
 
-    display.textContent = `${days}:${hours}:${minutes}:${seconds}:${miliseconds}`;
+function update() {
+  const currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
+  let days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24))
+    .toString()
+    .padStart(2, 0);
+  let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
+    .toString()
+    .padStart(2, 0);
+  let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60)
+    .toString()
+    .padStart(2, 0);
+  let seconds = Math.floor((elapsedTime / 1000) % 60)
+    .toString()
+    .padStart(2, 0);
+  let miliseconds = Math.floor((elapsedTime % 1000) / 10)
+    .toString()
+    .padStart(2, 0);
+
+  display.textContent = `${days}:${hours}:${minutes}:${seconds}:${miliseconds}`;
 }
